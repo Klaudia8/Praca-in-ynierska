39d983592de49073bae850ab0a5346165df4e4c0
Klaudia <klaudia_gorys@wp.pl>
2016-10-16 22:17:24 +0200

    ROzszerzenie pliku

--- initial_analysis.R
+++ initial_analysis.R
@@ -1,18 +1,22 @@
 length(start_time) # liczba rekordow - 842608
 sum(time_diff==0) # 19726
 
-# analiza zwrotów i wypozyczen w innych lokalizacjach i czasie = 0
-sum(start_location != end_location & time_diff==0) # 263
-bike[which(start_location != end_location & time_diff==0),2:3] # daty tych przypadków 
-bike[which(start_location != end_location & time_diff==0),4:5] # ich lokalizacje
+mean_time <- mean(time_diff)
+med_time <- median(time_diff)
 
-# analiza zwrotów rowerów
-sum(start_location == end_location & time_diff==0) # 19463
-bike_return <- bike[which(start_location == end_location & time_diff==0),] #rowery o czasie = 0 i tych samych lokalizacjach
-after_bike_return <- bike[which(start_location == end_location & time_diff==0)+1,] # rekord nastepujacy po zwrocie roweru
+# HISTOGRAMY
+# histogram wszytskich roznic w czasie
+hist(time_diff,main="Histogram czasu wypożyczeń",xlab="Różnica czasu wypożyczeń a zwrotów",ylab="Liczba wypożyczeń rowerów",breaks=200,freq=TRUE,border="blue")
 
-sum(bike_return[,4] == after_bike_return[,4]) # 3907 - te samy lokalizacje wypozyczenia przy zwrocie roweru i zaraz po nim wśród wszystkich zwrotów
-bike_return[]
+# po usunięciu czasu większego niż, np. 100 min
+time_diff_less <- time_diff[-which(time_diff>100)]
+hist(time_diff_less,main="Histogram ilościowy czasu wypożyczeń",xlab="Różnica czasu wypożyczeń a zwrotów",ylab="Ilość wypożyczeń",breaks=100,freq=TRUE,border="blue")
 
-bike_return[1:15,]
-after_bike_return[1:15,]
+# ile wypozyczen dla roznic w czasie (dla kazdej minuty - od 0 do 1192)
+how_long_bike_borrowed <- dbGetQuery(R_SQL,"SELECT timestampdiff(MINUTE,start_time,end_time) ile_time_diff,count(start_time)
+                  FROM rowery
+                  GROUP BY ile_time_diff;")
+
+# maksymalny czas wypozyczenia roweru
+max(how_long_bike_borrowed[,1]) # 1192
+
